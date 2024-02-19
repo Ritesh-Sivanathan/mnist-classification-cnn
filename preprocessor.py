0ece22dc6ee962ac9e538926440222b711e78ce8
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2024-02-19 14:11:09 -0500

    initial commit

---
+++ preprocessor.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow.python.keras.layers import Concatenate
+import os
+
+def process_image(img_paths: list[str]) -> list[tf.float32, int]:
+    
+    processed_images = []
+    labels = []
+
+    for directory in img_paths:
+
+        os.chdir(f'{os.getcwd()}\\{directory}')
+
+        for index, unprocessed_img in enumerate(os.listdir()):
+            if index <= 50:
+                image = tf.io.read_file(unprocessed_img)
+                processed_image = tf.io.decode_image(image, channels=1) # mnist dataset is greyscale so rather than 3 channels (rgb) you use just one 
+                processed_image = tf.image.resize(processed_image, [28, 28]) # all images in mnist datset are 28x28 (i believe)
+                processed_image = tf.cast(processed_image, tf.float32) / 255.0
+                # processed_image = tf.image.random_flip_left_right(processed_image, seed=42)
+                # processed_image = tf.image.random_brightness(processed_image, max_delta=0.2)                
+
+                processed_images.append(processed_image)
+                
+                labels.append(int(os.getcwd()[-1])) ### appends last digit of the directory name since that's the corresponding number
+
+        os.chdir('[MNIST HND DIRECTORY]')
+
+        # augmentation after the model is complete so my model is superior 😎
+
+
+    return processed_images, labels
+
+
+    
+        
+
+
---
+++ train.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from preprocessor import process_image
+import os
+
+# ------------ processing image ------------
+
+path_list = []
+
+os.chdir(f'{os.getcwd()}\\numbers\\mnist_png\\Hnd')
+for path in os.listdir():
+    path_list.append(path)
+
+processed_images, labels = process_image(path_list)
+
+# -------------------------------------------
+
+dataset = tf.data.Dataset.from_tensor_slices((processed_images, labels))
+batch_size = 3
+dataset = dataset.shuffle(len(processed_images))  # shuffleshuffle shuffleshuffle shuffleshuffle
+dataset = dataset.batch(batch_size)  # splitting batches
+print(len(dataset))
+
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape = (28,28,1)),  ### h,w,channels
+    MaxPooling2D((2,2,)),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D((2,2)),
+    Conv2D(64, (3,3), activation='relu'),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile( # https://keras.io/api/optimizers/adam/
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'],
+) 
+
+dataset = dataset.shuffle(len(processed_images))
+valsize = int(0.2*len(processed_images))
+training_data = dataset.skip(valsize)
+val_dataset = dataset.take(valsize)
+
+history = model.fit(training_data, epochs=10, validation_data=val_dataset)
+test_loss, test_acc = model.evaluate(dataset)
+
+print(f'ACCURACY: {test_acc}')
+
+os.chdir(f'[DIRECTORY]')
+model.save('model\\mnist_model.h5')
+
