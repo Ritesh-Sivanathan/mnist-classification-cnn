433482752aa03f76a55bfb516c685aeed651396d
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2024-02-24 21:52:49 -0500

    added image augmentation

--- code/preprocessor.py
+++ code/preprocessor.py
@@ -12,19 +12,22 @@ def process_image(img_paths: list[str]) -> list[tf.float32, int]:
         os.chdir(f'{os.getcwd()}\\{directory}')
 
         for index, unprocessed_img in enumerate(os.listdir()):
-            if index <= 50:
+            if index <= 250:
                 image = tf.io.read_file(unprocessed_img)
                 processed_image = tf.io.decode_image(image, channels=1) # mnist dataset is greyscale so rather than 3 channels (rgb) you use just one 
                 processed_image = tf.image.resize(processed_image, [28, 28]) # all images in mnist datset are 28x28 (i believe)
                 processed_image = tf.cast(processed_image, tf.float32) / 255.0
-                # processed_image = tf.image.random_flip_left_right(processed_image, seed=42) augmentation just cuz, commented for now
-                # processed_image = tf.image.random_brightness(processed_image, max_delta=0.2)                
+                processed_image = tf.image.random_flip_left_right(processed_image, seed=42)
+                processed_image = tf.image.random_brightness(processed_image, max_delta=0.2)                
 
                 processed_images.append(processed_image)
                 
                 labels.append(int(os.getcwd()[-1])) ### appends last digit of the directory name since that's the corresponding number
 
-        os.chdir('[MNIST HND DIRECTORY]')
+        os.chdir('C:\\Users\\User\\Documents\\Development\\AI\\identify numbers\\numbers\\mnist_png\\Hnd') ####################################################################################################
+
+        # augmentation after the model is complete so my model is superior 😎
+
 
     return processed_images, labels
 
