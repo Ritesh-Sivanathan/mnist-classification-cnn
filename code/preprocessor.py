4b36d8fe7a7213df7c2ff81de10cea059cd5eb91
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2024-12-09 10:12:31 -0500

    fixed comments

--- code/preprocessor.py
+++ code/preprocessor.py
@@ -9,17 +9,17 @@ def process_image(img_paths: list[str]) -> list[tf.float32, int]:
 
     for directory in img_paths:
 
-        for index, unprocessed_img in enumerate(os.listdir(f'../data/mnist/Hnd/{directory}')): # not sure if this is the correct way for the mnist dataset file struct but ...
+        for index, unprocessed_img in enumerate(os.listdir(f'../data/mnist/Hnd/{directory}')):
                 image = tf.io.read_file(unprocessed_img)
-                processed_image = tf.io.decode_image(image, channels=1) # mnist dataset is greyscale so rather than 3 channels (rgb) you use just one 
-                processed_image = tf.image.resize(processed_image, [28, 28]) # all images in mnist datset are 28x28 (i believe)
+                processed_image = tf.io.decode_image(image, channels=1) # Mnist dataset is greyscale so rather than 3 channels (rgb) you use just one 
+                processed_image = tf.image.resize(processed_image, [28, 28]) 
                 processed_image = tf.cast(processed_image, tf.float32) / 255.0
                 processed_image = tf.image.random_flip_left_right(processed_image, seed=42)
                 processed_image = tf.image.random_brightness(processed_image, max_delta=0.2)                
 
                 processed_images.append(processed_image)
                 
-                labels.append(int(directory[-1])) ### appends last digit of the directory name since that's the corresponding number
+                labels.append(int(directory[-1])) # Appends last digit of the directory name since that's the corresponding number
 
     return processed_images, labels
 
